77255a2d1b9f34c9b1e8de3516a0835627713eed
oyyb-lianxi <1170989239@qq.com>
2025-06-18 20:09:32 +0800

    Create TeacherSubject.java

---
+++ src/main/java/com/example/model/domain/TeacherSubject.java
@@ -0,0 +1,14 @@
+package com.example.model.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor  // 满参构造方法
+@NoArgsConstructor  // 无参构造方法
+public class TeacherSubject {
+    private Long id;
+    private Long teacherId;
+    private String subject;
+}
