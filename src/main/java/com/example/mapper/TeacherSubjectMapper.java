54c01d2f53070e8ba07479069d5a96d981be600d
oyyb-lianxi <1170989239@qq.com>
2025-06-18 20:40:27 +0800

    Create TeacherSubjectMapper.java

---
+++ src/main/java/com/example/mapper/TeacherSubjectMapper.java
@@ -0,0 +1,13 @@
+package com.example.mapper;
+
+import com.example.model.domain.Appointment;
+import com.example.model.domain.Notification;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface TeacherSubjectMapper {
+  
+}
