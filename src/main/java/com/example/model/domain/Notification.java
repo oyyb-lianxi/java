c9c175c13610bc634fcc33ab7fe10a4921e3f3cf
oyyb-lianxi <1170989239@qq.com>
2025-06-18 20:10:35 +0800

    Create Notification.java

---
+++ src/main/java/com/example/model/domain/Notification.java
@@ -0,0 +1,15 @@
+package com.example.model.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor  // 满参构造方法
+@NoArgsConstructor  // 无参构造方法
+public class Notification {
+    private Long id;
+    private Long userId;
+    private String message;
+    private String created;
+}
