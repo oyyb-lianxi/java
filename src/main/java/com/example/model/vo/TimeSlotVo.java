a3296f5980953372772a484e5820e38bd763b84f
oyyb-lianxi <1170989239@qq.com>
2025-06-19 15:59:58 +0800

    Create TimeSlotVo.java

---
+++ src/main/java/com/example/model/vo/TimeSlotVo.java
@@ -0,0 +1,35 @@
+import java.time.LocalDateTime;
+
+public class TimeSlotVo {
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+    public TimeSlot(LocalDateTime start, LocalDateTime end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeSlotVo{" +
+                "start=" + start +
+                ", end=" + end +
+                '}';
+    }
+}
