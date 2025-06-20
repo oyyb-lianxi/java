ad9250da975006ccec47ce3dfd41c1a1c925c4a6
oyyb-lianxi <1170989239@qq.com>
2025-06-20 15:21:38 +0800

    Update Notification.java

--- src/main/java/com/example/model/domain/Notification.java
+++ src/main/java/com/example/model/domain/Notification.java
@@ -8,8 +8,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor  // 满参构造方法
 @NoArgsConstructor  // 无参构造方法
 public class Notification extends BasePojo{
-    private Long id;
-    private Long userId;
+    private String id;
+    private String teacherId;
+    private String studentId;
     private String message;
     private String created;
 }
