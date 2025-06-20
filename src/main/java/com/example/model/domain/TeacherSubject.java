f20beb1b7a345bf989ccb0569e4c7f2fa094b2cc
oyyb-lianxi <1170989239@qq.com>
2025-06-20 16:29:52 +0800

    Update TeacherSubject.java

--- src/main/java/com/example/model/domain/TeacherSubject.java
+++ src/main/java/com/example/model/domain/TeacherSubject.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor  // 满参构造方法
 @NoArgsConstructor  // 无参构造方法
 public class TeacherSubject {
-    private Long id;
-    private Long teacherId;
+    private String id;
+    private String userId;
     private String subject;
 }
