80247d9759e7f04d38d8981a4fe3fb4127e92bca
oyyb-lianxi <1170989239@qq.com>
2025-06-27 10:57:51 +0800

    Update AppointmentController.java

--- src/main/java/com/example/controller/AppointmentController.java
+++ src/main/java/com/example/controller/AppointmentController.java
@@ -56,22 +56,7 @@ public class AppointmentController {
        return result;
    }
 
-   /**
-     * 老师删除可以预约时间
-     * @param
-     * @return
-     */
-   @PostMapping("/deleteTeacherTimeById/{id}")
-   public Result deleteTeacherTimeById(@PathVariable Long id) {
-       Result result=new Result();
-           if(teacherTimeService.deleteTeacherTimeById(id)){
-               result.setCode(200);
-               result.setMsg("teacherTime删除成功");
-               return result;
-           }
-               result.setMsg("删除失败，请联系管理员");
-           return result;
-   }
+
 
    /**
      * 老师接收预约
