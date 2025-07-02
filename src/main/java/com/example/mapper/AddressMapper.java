e77b6d4c9ce468ef44c731d1b762bc6aa2a65917
oyyb-lianxi <1170989239@qq.com>
2025-07-02 15:58:12 +0800

    Update AddressMapper.java

--- src/main/java/com/example/mapper/AddressMapper.java
+++ src/main/java/com/example/mapper/AddressMapper.java
@@ -4,6 +4,7 @@ package com.example.mapper;
 
 import com.example.model.domain.Admin;
 import com.example.model.domain.User;
+import com.example.model.domain.Adress;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -13,5 +14,15 @@ import java.util.List;
 @Mapper
 public interface AddressMapper {
 
-  
+
+    @Select("SELECT * FROM adress  WHERE userId = #{userId}")
+    Adress getUserAdressById(@Param("userId") String userId);
+
+    int saveUserAdress(Address adress);
+
+    int updateAdressById(Address adress);
+
+    int deleteUserAdressById(Address adress);
+
+    ListL<Address> getUsersAdressByConditions(Address adress);
 }
