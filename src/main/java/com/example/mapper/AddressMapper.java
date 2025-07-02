d451254619e4faf365766f54eceefe538a22b590
oyyb-lianxi <1170989239@qq.com>
2025-07-02 15:54:52 +0800

    Update AddressMapper.java

--- src/main/java/com/example/mapper/AddressMapper.java
+++ src/main/java/com/example/mapper/AddressMapper.java
@@ -1,1 +1,17 @@
 
+package com.example.mapper;
+
+
+import com.example.model.domain.Admin;
+import com.example.model.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface AddressMapper {
+
+  
+}
