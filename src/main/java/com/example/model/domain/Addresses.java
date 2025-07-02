228276f7b7fbe6a54fc025f40057df03aa7f8347
oyyb-lianxi <1170989239@qq.com>
2025-07-02 10:53:12 +0800

    Update Addresses.java

--- src/main/java/com/example/model/domain/Addresses.java
+++ src/main/java/com/example/model/domain/Addresses.java
@@ -1,4 +1,49 @@
+package com.example.model.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+//后台系统的管理员对象
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Addresses  extends BasePojo {
+    /**
+     * id
+     */
+    private Long id;
+    /**
+     * 用户openID
+     */
+    private String userId;
+    /**
+     * 用户类型
+     */
+    private String userType;
+    /**
+     * 省
+     */
+    private String province;
+
+    /**
+     * 市
+     */
+    private String city;
+
+      /**
+     * 县（区）
+     */
+    private String district;
+    
+    /**
+     * 详细地址
+     */
+    private String detailedAddress;
+
+}
 
 
 
